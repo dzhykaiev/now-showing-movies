0578b8e4d96cc62c543abc39a84f360ef9bde8fc
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-04-10 23:37:36 +0300

    init files

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./containers/App/App";
+import "./scss/main.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <div>Init</div>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
