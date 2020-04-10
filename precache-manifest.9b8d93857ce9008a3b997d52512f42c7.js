ff6977357ea5e52b45460cc1b3b42bf0956eb536
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-04-11 00:10:31 +0300

    Updates

---
+++ precache-manifest.9b8d93857ce9008a3b997d52512f42c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a38805aa9db29837cbac7f2b2454185",
+    "url": "/now-showing-movies/index.html"
+  },
+  {
+    "revision": "03dcd2a0ace9a5cedf58",
+    "url": "/now-showing-movies/static/css/main.65ec7f47.chunk.css"
+  },
+  {
+    "revision": "6bbdcdd84d9bfb1c4a7d",
+    "url": "/now-showing-movies/static/js/2.160c0a79.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "/now-showing-movies/static/js/2.160c0a79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03dcd2a0ace9a5cedf58",
+    "url": "/now-showing-movies/static/js/main.41ce595a.chunk.js"
+  },
+  {
+    "revision": "af9ad19018f705706009",
+    "url": "/now-showing-movies/static/js/runtime-main.57685f82.js"
+  },
+  {
+    "revision": "31d26327470d3d7f96f7bed3417fe659",
+    "url": "/now-showing-movies/static/media/logo.31d26327.svg"
+  }
+]);
