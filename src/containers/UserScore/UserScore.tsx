6c23e8f4adf2ab11e98ac4ac78d90379764bd9ba
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-04-11 19:09:37 +0300

    added debounce for userSelect

--- src/containers/UserScore/UserScore.tsx
+++ src/containers/UserScore/UserScore.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import RangeField from "../../components/RangeField/RangeField";
 import { useStores } from "../../hooks/use-stores";
+import { debounce } from "lodash";
 /**
  * Container that config RangeField component
  *
  */
 const UserScore: React.FC = (props): JSX.Element => {
   const { movieStore } = useStores();
-  const changeUserScore = movieStore.changeUserScore;
+  const changeUserScore = debounce(movieStore.changeUserScore, 300);
   return (
     <RangeField
       label="User Score"
