52db94a2e253e749aa35bbe2ff9fdfe7a549df16
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-04-13 14:53:45 +0300

    rid urls const and conditionals from api file

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,24 +1,7 @@
-/**
- * Here will be all api's stuff
- *
- */
-
 import axios from "axios";
 
-/**
- * Conditionally saving data in values, this is for usage mock data if used without api_key
- *
- */
-export const API_DEFAULT_URLS = process.env.REACT_APP_TMDb_API_KEY
-  ? ["/movie/now_playing", "/genre/movie/list"]
-  : ["/now_playing.json", "/genre.json"];
-
-const baseURL = process.env.REACT_APP_TMDb_API_KEY
-  ? "https://api.themoviedb.org/3/"
-  : "/";
-
 export const API = axios.create({
-  baseURL: baseURL,
+  baseURL: "https://api.themoviedb.org/3/",
 });
 
 API.interceptors.request.use(
